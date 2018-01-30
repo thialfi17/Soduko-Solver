d7fd2cdb0ba7a0677ae21780b4fd6e538770aa9d
thialfi17 <thialfi17@gmail.com>
2018-01-30 15:55:53 +0000

    Update main.cpp
    Actually added the current_p_values. Fixed the scan function.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main()
 	int * current_numbers[BOARD_SIZE];   //The current values of the numbers stored in the group we are looking at.
 	int p_values[BOARD_SIZE*BOARD_SIZE]; //Possible values for the cells on the board.
 	int * current_p_values[BOARD_SIZE];
-	int mask_sum, prev_mask_sum = 0;
+	int mask_sum = 0, prev_mask_sum = 0;
+	
+	for (int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++) {
+		p_values[i] = 0;
+	}
 	
 	read_board(board); 
 	
@@ -25,24 +29,26 @@ int main()
 	load_group(COLUMN, 2, current_numbers, board, current_p_values, p_values);
 	*current_numbers[2] = 3;*/
 	
-	while(mask_sum < 3 * (pow(2, BOARD_SIZE+1) - 1) * BOARD_SIZE)
+	while(mask_sum < 3 * (pow(2, BOARD_SIZE) - 1) * BOARD_SIZE)
 	{	
-		prev_mask_sum = mask_sum
+		prev_mask_sum = mask_sum;
+		mask_sum = 0;
 		for(int i = 0; i < BOARD_SIZE; i++)
 		{
 			load_group(BLOCK, i, current_numbers, board, current_p_values, p_values);
-			mask_sum += scan_group(c_numbers, current_p_values);
+			mask_sum += scan_group(current_numbers, current_p_values);
 			
 			load_group(ROW, i, current_numbers, board, current_p_values, p_values);
-			mask_sum += scan_group(c_numbers, current_p_values);
+			mask_sum += scan_group(current_numbers, current_p_values);
 			
 			load_group(COLUMN, i, current_numbers, board, current_p_values, p_values);
-			mask_sum += scan_group(c_numbers, current_p_values);
+			mask_sum += scan_group(current_numbers, current_p_values);
 		}
+		
 		if(mask_sum == prev_mask_sum)
 		{
 			cout << "Puzzle unsolvable!" << endl;
-			break;	
+			return 0;	
 		}
 	}
 	
@@ -55,37 +61,43 @@ int scan_group(int* c_num[BOARD_SIZE], int* c_p_val[BOARD_SIZE]) {
 	//Find the numbers missing in the group
 	for(int i = 0; i < BOARD_SIZE; i++)	//Finding missing numbers, updating possibilities mask.
 	{					//Using mask, if value is 0, that number is possible.
-		if(*c_num == 0) {
+		if(*c_num[i] == 0) {
 			continue;
 		}
 		int bit = 1<<( *c_num[i]-1);
-		if (temp_mask &= bit) {
-			cout << "The number: " << *c_num[i] << " has occurred before in this group!";
+		
+		if ((temp_mask & bit) == bit) {
+			 cout << "The number: " << *c_num[i] << " has occurred before in this group!" << endl;
 		}
 		temp_mask |= bit;
 	}
 	for(int i = 0; i < BOARD_SIZE; i++)
 	{
-		if(*c_num == 0)
+		if(*c_num[i] == 0)
 		{
 			*c_p_val[i] |= temp_mask;
 		}
 	}
-	
 	return temp_mask;
 }
 
 void load_group(GroupType t, int group, int* c_num[BOARD_SIZE], int (&board)[BOARD_SIZE*BOARD_SIZE],
-										int* c_p_val[BOARD_SIZE], int (&c_val)[BOARD_SIZE*BOARD_SIZE]) {
+										int* c_p_val[BOARD_SIZE], int (&p_val)[BOARD_SIZE*BOARD_SIZE]) {
 	for (int i = 0; i < BOARD_SIZE; i++) {
 		if (t == ROW) {
 			c_num[i] = &board[i + group*BOARD_SIZE];
+			c_p_val[i] = &p_val[i + group*BOARD_SIZE];
 		}
 		else if (t == COLUMN) {
 			c_num[i] = &board[group + i * BOARD_SIZE];
+			c_p_val[i] = &p_val[group + i * BOARD_SIZE];
 		}
 		else if (t == BLOCK) {
 			c_num[i] = &board[ (group % BLOCK_SIZE) * BLOCK_SIZE + 				// Calculates the initial horizontal position of the group
+							   ((group/BLOCK_SIZE) * BLOCK_SIZE * BOARD_SIZE) +  // Calculates the initial vertical position of the group
+							   (i / BLOCK_SIZE * BOARD_SIZE) + 					// Calculates the vertical position within the group
+							   (i % BLOCK_SIZE)];								// Calculates the horizontal position within the group
+			c_p_val[i] = &p_val[ (group % BLOCK_SIZE) * BLOCK_SIZE + 				// Calculates the initial horizontal position of the group
 							   ((group/BLOCK_SIZE) * BLOCK_SIZE * BOARD_SIZE) +  // Calculates the initial vertical position of the group
 							   (i / BLOCK_SIZE * BOARD_SIZE) + 					// Calculates the vertical position within the group
 							   (i % BLOCK_SIZE)];								// Calculates the horizontal position within the group
