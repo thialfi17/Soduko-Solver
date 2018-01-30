66c84ec212281585625b6853c3b255db3a455fcb
thialfi17 <thialfi17@gmail.com>
2018-01-30 12:10:39 +0000

    Update main.cpp
    Added print_board function

--- main.cpp
+++ main.cpp
@@ -10,18 +10,23 @@ const int BLOCK_SIZE = sqrt(BOARD_SIZE);
 
 void read_board(int (&board)[BOARD_SIZE*BOARD_SIZE]);
 void load_group(GroupType t, int group, int* current_numbers[BOARD_SIZE], int (&board)[BOARD_SIZE*BOARD_SIZE]);
+void print_board(int (&board)[BOARD_SIZE*BOARD_SIZE]);
 
 int main() 
 {
-  int board[BOARD_SIZE * BOARD_SIZE];
-  int * current_numbers[BOARD_SIZE];
-  read_board(board); 
-  
-  load_group(BLOCK, 2, current_numbers, board);
-  
-  cout << *current_numbers[2] << endl;
-  cout << board[0] << board[10] << board[80] << endl;
-  
+	int board[BOARD_SIZE * BOARD_SIZE];
+	int * current_numbers[BOARD_SIZE];
+	read_board(board); 
+	
+	load_group(BLOCK, 2, current_numbers, board);
+	cout << *current_numbers[2] << endl;
+	load_group(ROW, 2, current_numbers, board);
+	cout << *current_numbers[2] << endl;
+	*current_numbers[2] = 3;
+	load_group(COLUMN, 2, current_numbers, board);
+	cout << *current_numbers[2] << endl;
+	
+	print_board(board);
 }
 
 void load_group(GroupType t, int group, int* current_numbers[BOARD_SIZE], int (&board)[BOARD_SIZE*BOARD_SIZE]) {
@@ -40,18 +45,33 @@ void load_group(GroupType t, int group, int* current_numbers[BOARD_SIZE], int (&
 
 void read_board(int (&board)[BOARD_SIZE*BOARD_SIZE])
 {
-  FILE * f = fopen("puzzle.txt", "r");
- 
-  int count = 0;
-  while ( !feof(f) ) {
-    char c = getc(f);
-    
-    if ( c > 47 && c < 58 ) {
-     board[count++] = c - 48; 
+    FILE * f = fopen("puzzle.txt", "r");
+   
+    int count = 0;
+    while ( !feof(f) ) {
+        char c = getc(f);
+        
+        if ( c > 47 && c < 58 ) {
+            board[count++] = c - 48; 
+        }   
+        if (count >= BOARD_SIZE * BOARD_SIZE) {
+            break;
+        }
     }
-    if (count >= BOARD_SIZE * BOARD_SIZE) {
-      break;
-    }
-  }
-  fclose(f);
+    fclose(f);
 }
+
+void print_board(int (&board)[BOARD_SIZE*BOARD_SIZE]) {
+		for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+			std::cout << board[i];
+
+			if ((i + 1) % BLOCK_SIZE == 0)
+				std::cout << " ";
+			if ((i + 1) % BOARD_SIZE == 0) {
+				std::cout << std::endl;
+				if ((i + 1) % (BOARD_SIZE * BLOCK_SIZE) == 0) {
+					std::cout << std::endl;
+				}
+			}
+		}
+	}
