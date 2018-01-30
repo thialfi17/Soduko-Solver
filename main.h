48213af85c7cf24dc7f92a1f3fdaee3321dcb5a2
thialfi17 <thialfi17@gmail.com>
2018-01-30 15:56:27 +0000

    Update main.h
    Slightly modified scan_group

--- main.h
+++ main.h
@@ -6,6 +6,6 @@ const int BLOCK_SIZE = sqrt(BOARD_SIZE);
 
 void read_board(int (&board)[BOARD_SIZE*BOARD_SIZE]);
 void load_group(GroupType t, int group, int* c_num[BOARD_SIZE], int (&board)[BOARD_SIZE*BOARD_SIZE],
-										int* c_p_val[BOARD_SIZE], int (&c_val)[BOARD_SIZE*BOARD_SIZE]);
-int scan_group(int* c_num[BOARD_SIZE], int* c_p_val);
+										int* c_p_val[BOARD_SIZE], int (&p_val)[BOARD_SIZE*BOARD_SIZE]);
+int scan_group(int* c_num[BOARD_SIZE], int* c_p_val[BOARD_SIZE]);
 void print_board(int (&board)[BOARD_SIZE*BOARD_SIZE]);
